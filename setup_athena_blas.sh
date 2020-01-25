84997f0921addb20431e7a56b4a66fed600639b1
Leighton W. Wilson <leightonwilson@mac.com>
2020-01-25 17:50:06 -0500

    Making build work on athena

---
+++ setup_athena_blas.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/export/zimmerman/khoidang/local_mod/OpenBLAS/lib/:$LD_LIBRARY_PATH
+export CPATH=/export/zimmerman/khoidang/local_mod/OpenBLAS/include/:$CPATH
+export CMAKE_PREFIX_PATH=/export/zimmerman/khoidang/local_mod/OpenBLAS/
